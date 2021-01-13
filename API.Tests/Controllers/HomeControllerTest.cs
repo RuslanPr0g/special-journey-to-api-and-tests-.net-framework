2598fe6a85471623ecf4c71bf5ff7c401b14511b
RuslanPr0g <theruslan.prog@gmail.com>
2021-01-13 17:35:19 +0300

    initialize API and Tests projects

---
+++ API.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using API;
+using API.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+
+namespace API.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Home Page", result.ViewBag.Title);
+        }
+    }
+}
