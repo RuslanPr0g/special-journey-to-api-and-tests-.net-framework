45922129288d40d22d73b0f40cbf7ad2d8da071d
RuslanPr0g <theruslan.prog@gmail.com>
2021-01-13 19:06:30 +0300

    add people controller, Person class, remove values controller

---
+++ API/Controllers/PeopleController.cs
@@ -0,0 +1,71 @@
+﻿using API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API.Controllers
+{
+    public class PeopleController : ApiController
+    {
+        private List<Person> _people = new List<Person>
+        {
+             new Person
+             {
+                 ID = 0,
+                 FirstName = "John",
+                 LastName = "Smith"
+             },
+             new Person
+             {
+                 ID = 1,
+                 FirstName = "Dmitriy",
+                 LastName = "Amerados"
+             },
+             new Person
+             {
+                 ID = 2,
+                 FirstName = "William",
+                 LastName = "Shakespear"
+             },
+             new Person
+             {
+                 ID = 3,
+                 FirstName = "John",
+                 LastName = "Williamed"
+             },
+             new Person
+             {
+                 ID = 4,
+                 FirstName = "Will",
+                 LastName = "Smith"
+             },
+        };
+
+        // GET: api/People
+        public IEnumerable<Person> Get()
+        {
+            return _people;
+        }
+
+        // GET: api/People/5
+        public Person Get(int id)
+        {
+            return _people.Where(p => p.ID == id).FirstOrDefault();
+        }
+
+        // POST: api/People
+        public void Post(Person person)
+        {
+            _people.Add(person);
+        }
+
+        // DELETE: api/People/5
+        public void Delete(int id)
+        {
+            _people.RemoveAt(id);
+        }
+    }
+}
---
+++ API/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API.Models
+{
+    public class Person
+    {
+        public int ID { get; set; } = 0;
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+    }
+}
